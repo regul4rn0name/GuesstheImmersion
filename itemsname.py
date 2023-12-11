d5ada5cad422667e4722ac2c53ed0f9e07d94bee
regul4rn0name <komlyk07@gmail.com>
2023-12-11 20:10:03 +0100

    Using dname instead of tags

--- itemsname.py
+++ itemsname.py
@@ -5,21 +5,30 @@ import json
 def get_item_names(ids):
     # Fetch the item data from the Dota 2 constants repository
     url = "https://raw.githubusercontent.com/odota/dotaconstants/master/build/item_ids.json"
+    url2 = "https://raw.githubusercontent.com/odota/dotaconstants/master/build/items.json"
+    response2 = requests.get(url2)
     response = requests.get(url)
 
     if response.status_code == 200:
         item_data = response.json()
+        dname_data = response2.json()
         item_names = []
+        item_dnames = []
 
         # Append names to items using the provided IDs
         for item_id in ids:
             item_name = item_data.get(str(item_id), "Item not found")
             item_names.append(item_name)
 
-        return item_names
+        for item_tag in item_names:
+            item_dname = dname_data.get(str(item_tag), {}).get('dname', "Item not found")
+            item_dnames.append(item_dname)
+
+        return item_dnames
     else:
         print("Failed to fetch item data")
         return None
+
 
 def main():
     # Use specific_player_items as the array of item IDs
